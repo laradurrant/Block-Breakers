82a59d7c84e898e84bbb31e63e3f5e76530cbf4b
Lara <lara.durrant@gmail.com>
2018-01-13 20:03:48 -0800

    Initial commit

---
+++ Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour {
+
+	Vector3 paddlePosition;
+	float mousePos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		paddlePosition = new Vector3(0f, -6f, 0f);
+		mousePos = Input.mousePosition.x / Screen.width;
+		paddlePosition.x = mousePos * 19f;
+		this.transform.position = paddlePosition;
+		debugFunctions ();
+
+
+	}
+
+
+	void debugFunctions()
+	{
+
+		print (paddlePosition.x + " " + paddlePosition.y);
+		print ("Screen width: " + Screen.width * 5);
+		print ("Mouse x pos: " + Input.mousePosition);
+		print ("Mouse Position: " + mousePos);
+		print ("Paddle Position x: " + paddlePosition.x);
+
+	}
+
+
+
+}
